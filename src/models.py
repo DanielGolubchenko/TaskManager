bb9a7faf31567d907c2471e05fa67717ce4b342f
Daniel Golubchenko <oraclejavascriptcode@gmail.com>
2025-02-11 23:13:12 +0100

    Implement task modification and manipulation features

--- src/models.py
+++ src/models.py
@@ -2,152 +2,187 @@ import hashlib
 from datetime import datetime
 
 class Task:
-    # Default non-modifiable variables
+    """
+    Represents a task with attributes like title, priority, status, and due date.
+    Provides methods to modify, display, and manage tasks.
+    """
+
+    # Default non-modifiable priority and status values
     priority_defaults = ("Low", "Medium", "High", "Urgent")
     status_defaults = ("Pending", "In progress", "Completed", "Canceled")
 
-    # Default modifiable variable
+    # Default modifiable tags values
     tags_defaults = ["Work", "Studies", "Home", "Meetings", "Goals", "Reading", "Shopping", "Bills"]
 
-    def __init__(self,
-                 title,#🚧(unir match-case + modificable)🚧
-                 priority,#🚧(unir match-case y validacion input respecto defaults)🚧
-                 status,#🚧(unir match-case y validacion input respecto defaults)🚧
-                 description=None,#🚧(unir match-case + modificable)🚧
-                 due_date=None,#Opcional: DD/MM/YY. Hora opcional(Hour/min) 🚧(unir match-case + modificable)🚧
-                 created_at=None,#🚧(unir match-case)🚧
-                 updated_at=None,#🚧(unir match-case)🚧
-                 tags=None, # 🚧(unir match-case + opción para modificar directa y libremente)🚧
-                 task_id=None):#🚧(unir match-case)🚧
+    def __init__(self, title, priority, status, description=None, due_date=None, created_at=None, updated_at=None, tags=None, task_id=None):
         self.title = title
-        self._priority = priority
-        self._status = status
-
+        self.priority = priority
+        self.status = status 
         self.description = description if description is not None else []
         self.due_date = due_date if due_date is not None else []
-
-        self.created_at = created_at if created_at is not None else datetime.now()
-        self.updated_at = updated_at if updated_at is not None else datetime.now()
-
+        self.created_at = created_at if created_at is not None else datetime.now().strftime("%d/%m/%Y %H:%M")
+        self.updated_at = updated_at if updated_at is not None else datetime.now().strftime("%d/%m/%Y %H:%M")
         self.tags = tags if tags is not None else [] 
-
         self.task_id = task_id if task_id is not None else self.generate_task_id()
 
-    # Generates unique task_id at task creation.
     def generate_task_id(self):
+        """Operates only at task creation"""
         data = (self.title + str(self.created_at)).encode('utf-8')
         return hashlib.sha256(data).hexdigest()
-    
-    @property
-    def priority(self):
-        return self._priority
-    
-    # User-modifiable priority 🚧🚧
-    @priority.setter
-    def priority(self, value):
-        self._priority = value
 
-    @property
-    def status(self):
-        return self._status
-    
-    # User-modifiable status 🚧🚧
-    @status.setter
-    def status(self, value):
-        self._status = value
+    def __str__(self):
+        """Returns a formatted string representation of the task object, displaying all attributes and their values."""
+        task_info = vars(self)
+        info_str = "\n----TASK----\n"
+        for k, v in task_info.items():
+            if "_" in k:
+                k = k.replace("_", " ")
+            info_str += f"{k.capitalize()}: {v}\n"
+        return info_str
+
+    def modify_task(self):
+        """Allows the user to modify an attribute of the task."""
+
+        options = ["Title", "Priority", "Status", "Description", "Due_date", "Tags"]
+
+        print(f"\nThese are the modifiable options:")
+        for i, option in enumerate(options, start=1):
+            print(f"{i}. {option}")
+
+        # Prompt to select an attribute
+        attribute_index = TaskManager.get_valid_input("\nEnter the number (1-6) for your choice: ", valid_options=[str(i) for i in range(1, 7)])
+        
+        # Get the attribute name corresponding to the selected number
+        attribute_name = options[int(attribute_index) - 1].lower()
+        
+        # Receive modification value for the selected attribute
+        modification = TaskManager.attribute_validation(attribute_name)
+        
+        # Dynamically assign the new value to the corresponding attribute
+        setattr(self, attribute_name, modification)
+        
+        # Update the timestamp whenever a modification is made
+        self.updated_at = datetime.now().strftime("%d/%m/%Y %H:%M")
+
+        print("Task updated successfully.")
 
 class TaskManager:
+    """A class responsible for managing tasks, including creation, modification, deletion and filtering."""
 
     def __init__(self, name="task_manager", ):
         self.name = name
-        # Cumulative task variable
         self.tasks_list = []
 
     def add_task(self):
         print("Those entries with the symbol (*) are mandatory.")
 
-        title = TaskManager.validation("title")
-        priority = TaskManager.validation("priority")
-        status = TaskManager.validation("status")
-        description = TaskManager.validation("description")
-        due_date = TaskManager.validation("due_date")
-        tags = TaskManager.validation("tags")
+        title = TaskManager.attribute_validation("title")
+        priority = TaskManager.attribute_validation("priority")
+        status = TaskManager.attribute_validation("status")
+        description = TaskManager.attribute_validation("description")
+        due_date = TaskManager.attribute_validation("due_date")
+        tags = TaskManager.attribute_validation("tags")
 
         task = Task(title, priority, status, description, due_date, tags)
         self.tasks_list.append(task)
         print("Task added succesfully!")
 
     def show_tasks(self):
-        pass
+        tasks = self.tasks_list
 
-    def find_task(self):
-        # 🚧🚧asi pondriamos en accion la funcion task_filter. realizar condicional o funcion extra para validar los diferentes return de la funciion.
-        filtered_list = TaskManager.task_filter(self.tasks_list)
+        if len(tasks) >= 2:
+            # Ask if the user wants to filter the task list
+            by_filter = TaskManager.get_valid_input("Do you want to display the task list with a specific filter? (yes/no): ")
+            if by_filter == "yes":
+                # Apply filter; if None is returned, use an empty list
+                tasks = TaskManager.task_filter(tasks) or []
+        
+        # Print each task in the final list (filtered or not)
+        for task in tasks:
+            print(task)
+
+    def find_and_modify_or_delete_task(self):
+        """Finds a task and allows the user to modify or delete it."""
+
+        filtered_list = TaskManager.task_filter(self.tasks_list) or []
+        
+        if not filtered_list:
+            return 
     
-    def modify_task(self):
-        pass # 🚧🚧terminar los getter y setter privados en class Task
+        for task in filtered_list:
+            print(task)
 
-    def delete_task(self):
-        pass
+        action = TaskManager.get_valid_input("Do you want to modify or delete a task? (modify/delete/no): ", valid_options=["modify", "delete", "no"])
 
-    # Validates each posible task atribute input
+        if action in ["modify", "delete"]:
+            if len(filtered_list) == 1:
+                task = filtered_list[0]  # Directly select the only task
+                print(f"\nSelected task:\n{task}") 
+            else:
+                print("\nSelect a task to proceed:")
+                for task in filtered_list:
+                    print(task) 
+
+                task_ids = [task.task_id for task in filtered_list]  # Get task IDs
+                task_id = TaskManager.get_valid_input("Enter the ID of the task: ", valid_options=task_ids)
+                task = next(task for task in filtered_list if task.task_id == task_id)  # Find the selected task
+
+            if action == "modify":
+                task.modify_task()
+            else:
+                self.tasks_list.remove(task)
+                print("Task deleted successfully.")
+    
     @staticmethod
-    def validation(value):
+    def attribute_validation(value):
+        """Validates each posible task atribute input"""
+
         match value:
             case "title":
                 return TaskManager.get_valid_input("\nTitle(*): ")
 
             case "priority":
-                # Display available options for priority selection
                 print(f"\nOptions -> {Task.priority_defaults}")
                 return TaskManager.get_valid_input("Priority(*): ", valid_options=["low", "medium", "high", "urgent"]).capitalize()
 
             case "status":
-                # Display available options for status selection
                 print(f"\nOptions -> {Task.status_defaults}")
                 return TaskManager.get_valid_input("Status(*): ", valid_options=["pending", "in progress", "completed", "canceled"]).capitalize()
             
             case "description":
-                # Get the task description (optional). If empty, return None.
                 return TaskManager.get_valid_input("\nDescription (press 'Enter' to skip): ", allow_empty=True)
             
             case "due_date":
-                # Prompt user for a due date (Optional and can be just a date or date with time)
                 return TaskManager.get_valid_input("\nEnter due date (DD-MM-YYYY) or (DD-MM-YYYY HH:MM), or press 'Enter' to skip: ", is_date=True, allow_empty=True)
 
             case "tags":
-                # Initialize an empty list to store the selected tags
                 selected_tags = []
 
                 while True:
-                    # Presenting the available options for tag management
                     print(f"\nOptions -> {Task.tags_defaults}")
                     print("1. Choose a default tag")
                     print("2. Modify a default tag")
                     print("3. Create a new tag")
                     print("4. Skip (no tags)")
                    
-                    # Ensure the user input is one of the valid options (1-4)
                     choice = TaskManager.get_valid_input("\nEnter the number (1-4) for your choice: ", valid_options=["1", "2", "3", "4"])
                     
                     # Choice execution
                     match choice:
                         case "1":
-                            # Ask the user to select one of the default tags
                             print(f"\nOptions -> {Task.tags_defaults}")
                             user_input = TaskManager.get_valid_input("Tag: ", valid_options=[tag.lower() for tag in Task.tags_defaults]).capitalize()
-                            # Tag upload
-                            selected_tags.append(user_input)
+                            
+                            selected_tags.append(user_input) # Tag upload
                             print("Tag uploaded.")
-                            # Asking for more tags
+
                             add_more = input("\nDo you want to add another tag? (yes/no): ").strip().lower()
                             if add_more != "yes": return selected_tags
 
                         case "2":
-                            # Ask the user to select and modify a default tag
                             print(f"\nOptions -> {Task.tags_defaults}")
-                            # Validate input and ensure the selected tag is in the defaults
                             user_input = TaskManager.get_valid_input("Tag: ", valid_options=[tag.lower() for tag in Task.tags_defaults])
+                            
                             # Proceed to modify the selected tag
                             while True:
                                 change = TaskManager.get_valid_input("What's the new tag version?: ")
@@ -155,10 +190,13 @@ class TaskManager:
                                 print(f"New tag version: '{change}'")
                                 confirmation = input("Are you sure you want this to be the new version of the tag? (yes/no): ").strip().lower()
                                 if confirmation == "yes":
+                                    
                                     # Find original tag
                                     original_tag = next(tag for tag in Task.tags_defaults if tag.lower() == user_input.lower())
+                                    
                                     # Find original tag index
                                     index = Task.tags_defaults.index(original_tag)
+                                    
                                     # Modifying original tag
                                     Task.tags_defaults[index] = change
                                     selected_tags.append(change)
@@ -175,8 +213,7 @@ class TaskManager:
                                 print(f"New tag version: '{new}'")
                                 confirmation = input("Are you sure you want this to be the new tag? (yes/no): ").strip().lower()
                                 if confirmation == "yes":
-                                    # Add the new tag to the list of default tags
-                                    Task.tags_defaults.append(new)
+                                    Task.tags_defaults.append(new) # Add the new tag to the list of default tags
                                     selected_tags.append(new)
                                     print("Tag uploaded.")
                                     break
@@ -189,29 +226,27 @@ class TaskManager:
 
     @staticmethod
     def task_filter(tasks_list):
-        # Create a copy of the tasks_list to manipulate without affecting the original list
-        filtered_list = tasks_list[:]
+        """Filters tasks based on specified criteria such as priority, status, and tags."""
 
-        # Define the available filter options
-        options = ["title", "priority", "status", "due_date", "created_at", "tags"]
+        filtered_list = tasks_list[:]
+        options = ["Title", "Priority", "Status", "Due date", "Created_at", "Tags"]
         
-        # Function to filter tasks based on a user's input for a specific attribute
         def match_locator(user_input, attribute, filtered_list):
+            """Searches for a match between the user's input and the attribute value of each task in the filtered list."""
+
             filtered_list = [task for task in filtered_list if user_input in str(getattr(task, attribute, "")).lower()]
             if filtered_list:
-                print("Match achieved.")
+                print("Match achieved.\n")
                 return filtered_list
             else:
-                print("No match was achieved.")
+                print("No match was achieved.\n")
                 return None
 
-        # Display the filter options to the user
         print(f"\nThese are the possible filter options:")
         for i, option in enumerate(options, start=1):
             print(f"{i}. {option}")
 
-        # Ask the user to select a filter option (1-6)
-        choice = TaskManager.get_valid_input("\nEnter the number (1-6) for your choice: ", valid_options=["1", "2", "3", "4", "5", "6"])
+        choice = TaskManager.get_valid_input("\nEnter the number (1-6) for your choice: ", valid_options=[str(i) for i in range(1, 7)])
 
         # Perform the filtering action based on the user's choice
         match choice:
@@ -224,13 +259,13 @@ class TaskManager:
             case "2":
                 print(f"\nOptions -> {Task.priority_defaults}")
                 user_input = TaskManager.get_valid_input("\nWhat priority would you like to search for? ", valid_options=["low", "medium", "high", "urgent"])
-                return match_locator(user_input, "_priority", filtered_list)
+                return match_locator(user_input, "priority", filtered_list)
 
             # Status filter
             case "3":
                 print(f"\nOptions -> {Task.status_defaults}")
                 user_input = TaskManager.get_valid_input("\nWhat status would you like to search for? ", valid_options=["pending", "in progress", "completed", "canceled"])
-                return match_locator(user_input, "_status", filtered_list)
+                return match_locator(user_input, "status", filtered_list)
 
             # Due date filter
             case "4":
@@ -248,24 +283,29 @@ class TaskManager:
                 user_input = TaskManager.get_valid_input("\nWhat tag would you like to search for? ", valid_options=[tag.lower() for tag in Task.tags_defaults])
                 return match_locator(user_input, "tags", filtered_list)
 
-    # Function to get valid input from the user, handles empty input, invalid options, and date format
     @staticmethod
     def get_valid_input(prompt, valid_options=None, is_date=False, allow_empty=False):
+        """Function to get valid input from the user, handles empty input, invalid options, and date format"""
+
         while True:
             try:
                 user_input = input(prompt).strip().lower()
+                
                 # If empty input is allowed and the user provides an empty input, return None
                 if allow_empty and not user_input:
                     return None
+                
                 # Check if the input is empty and raise an error
                 if not user_input:
                     raise EmptyError()
+                
                 # Handle date input and convert to appropriate format
                 if is_date:
                     if " " in user_input:
                         return datetime.strptime(user_input, "%d-%m-%Y %H:%M")
                     else:
                         return datetime.strptime(user_input, "%d-%m-%Y").date()
+                
                 # Validate if the input matches one of the valid options
                 if valid_options and user_input not in valid_options:
                     raise NotAnOptionError()
@@ -278,7 +318,7 @@ class TaskManager:
                 print("Invalid format. Use 'DD-MM-YYYY' or 'DD-MM-YYYY HH:MM'.")
 
 class EmptyError(Exception):
-    # user entered an empty input, prompt for valid input
+    """Manages empty inputs"""
     def __init__(self, message="This slot can't be empty."):
         super().__init__(message)
         self.message = message
@@ -287,7 +327,7 @@ class EmptyError(Exception):
         return self.message
 
 class NotAnOptionError(Exception):
-    # user entered an invalid option, prompt for valid input
+    """Manages invalid options"""
     def __init__(self, message="That's not a valid option."):
         super().__init__(message)
         self.message = message
