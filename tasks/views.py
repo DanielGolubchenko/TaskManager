69860942e59f3aefd0fe6d9520fcc4f4a91b1078
Daniel Golubchenko <oraclejavascriptcode@gmail.com>
2025-03-18 18:58:09 +0100

    Implement user login functionality

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from .forms import CustomUserCreationForm
 
 
@@ -20,3 +20,20 @@ def register(request):
     else:
         form = CustomUserCreationForm()
     return render(request, 'register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login.html', {'form': form})
