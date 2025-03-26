1e401b7e9af801096d830206ca9b921434f5b9fd
Daniel Golubchenko <oraclejavascriptcode@gmail.com>
2025-03-26 16:11:27 +0100

    .

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -25,9 +25,7 @@ SECRET_KEY = 'django-insecure-q4wsvfq-ei+6f=^+(^)1)(hw1^4ivp&cr#e#$_=gk75izivgd@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ 'localhost', '127.0.0.1', 'golubchenko-taskmanager.up.railway.app' ]
-
-CSRF_TRUSTED_ORIGINS = [ 'https://golubchenko-taskmanager.up.railway.app' ]
+ALLOWED_HOSTS = ['taskmanager-68d9.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
