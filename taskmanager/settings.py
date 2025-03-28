2f22c11080fce908b7ca46f30e191a9b5ed3207e
Daniel Golubchenko <oraclejavascriptcode@gmail.com>
2025-03-28 11:28:13 +0100

    sqlite db

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -81,7 +81,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
 }
 
 
